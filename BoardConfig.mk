098104e00751d6b010fb7d77812707b821f25cb7
Kunmun <kunmun@aospa.co>
2022-11-02 05:03:22 +0000

    dubai: BoardConfig: Define kernel configuration
    Change-Id: I847fee43fcb11b3dd9267eefad76baa53e3e625b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,6 +60,7 @@ BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1 androidboot.hab.csv=8
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_CMDLINE += androidboot.hab.product=dubai
 BOARD_KERNEL_PAGESIZE    := 4096
+KERNEL_DEFCONFIG := dubai-qgki_defconfig
 
 # Partitions - A/B
 AB_OTA_PARTITIONS := boot dtbo odm product system system_ext vendor vendor_boot vbmeta vbmeta_system
