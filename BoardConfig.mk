4eadb089fabee0636e8af8192d9a9dfce133cfd6
Kunmun <kunmun@aospa.co>
2022-11-22 23:18:56 +0530

    dubai: BoardConfig: Define OTA Asserts
    Change-Id: I4162516975cf1ba209be6ecf1c973ba8a94513a0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,6 +63,9 @@ BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_RAMDISK_USE_LZ4 := true
 KERNEL_DEFCONFIG := dubai-qgki_defconfig
 
+# OTA Assert
+TARGET_OTA_ASSERT_DEVICE := dubai
+
 # Partitions - A/B
 AB_OTA_PARTITIONS := boot dtbo odm product system system_ext vendor vendor_boot vbmeta vbmeta_system
 AB_OTA_UPDATER := true
