7e459d30e687fd3f57f566974ed8f0b35eb9fcc0
Kunmun <kunmun@aospa.co>
2022-12-16 17:01:07 +0530

    dubai: BoardConfig: Redefine kernel cmdline as per our requirement
    Change-Id: Ia4c4b817c36747d603ee0ee470ab8ea1c9549fba

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,8 +45,16 @@ BOARD_INCLUDE_RECOVERY_DTBO := true
 # Kernel
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_DO_NOT_STRIP_VENDOR_MODULES := true
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=0 loop.max_part=7 cgroup.memory=nokmem,nosocket pcie_ports=compat loop.max_part=7 iptable_raw.raw_before_defrag=1 ip6table_raw.raw_before_defrag=1
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
+BOARD_KERNEL_CMDLINE += androidboot.memcg=1 lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237 androidboot.hab.cid=50
+BOARD_KERNEL_CMDLINE += service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE += swiotlb=0 loop.max_part=7 cgroup.memory=nokmem,nosocket
+BOARD_KERNEL_CMDLINE += pcie_ports=compat iptable_raw.raw_before_defrag=1
+BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1 androidboot.hab.csv=8
+BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
+BOARD_KERNEL_CMDLINE += androidboot.hab.product=dubai
 BOARD_KERNEL_PAGESIZE    := 4096
 
 # Partitions - A/B
