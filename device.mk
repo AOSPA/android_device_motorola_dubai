da847e001983e19af6fff980edf345110eee4e86
Kunmun <kunmun@aospa.co>
2023-03-10 19:50:19 +0530

    dubai: Makefile: Enable flag to build sensors sub HAL
    Change-Id: I41c83a3e64f30df9dc08c5441f6976b1b2f2233c

--- device.mk
+++ device.mk
@@ -199,6 +199,8 @@ TARGET_COMMON_QTI_COMPONENTS := all
 TARGET_USES_RRO := true
 
 # Sensors
+TARGET_BUILDS_OSS_SENSORS_SUBHAL := true
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml \
