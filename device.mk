a05febd872a512212c2e2b2a0bd6074f4031e95a
Kunmun <kunmun@aospa.co>
2023-03-11 07:10:30 +0530

    dubai: overlay: Frameworks: Set S1RD32.55-106-3 build fingerprint
    Change-Id: Ie6b4c302b42e238f65927cdf6d6e8ea9e3a358d4

--- device.mk
+++ device.mk
@@ -276,6 +276,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworksResDubai \
+    FrameworksResDubaiAOSPA \
     SettingsResDubai \
     SystemUIResDubai \
     WifiResTargetDubai
