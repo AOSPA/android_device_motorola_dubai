6c778a8034c74dc36b3fad6068749291a23f89ec
Mukul Dhir <dhirmukul3@gmail.com>
2023-03-14 23:38:11 +0000

    dubai: Drop powerhal package declaration
     • qti extension is not appended in our powerhal
     • Correct packages will be automatically built via [1].
    
    Reference:
    [1] https://github.com/AOSPA/android_device_qcom_common/blob/5fd9daca339d5174e42ae475200d7bc568c8a793/common.mk#L159
    
    Change-Id: I01326bc294f5e091d662106c09ecc40466156898

--- device.mk
+++ device.mk
@@ -300,10 +300,6 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
-
 # QMI
 PRODUCT_PACKAGES += \
     libjson
