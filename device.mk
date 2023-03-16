8195bed7563286e1e410b90089ca6e748735e883
Kunmun <kunmun@aospa.co>
2023-03-16 18:04:53 +0530

    dubai: Inherit common st nfc makefile definitions
    Change-Id: Ia046927c15d21d401c30184c05b7d4c8b7f4f7d9

--- device.mk
+++ device.mk
@@ -252,18 +252,15 @@ PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
 # NFC
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
-    frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml
+$(call inherit-product, hardware/st/nfc/nfc_vendor_product.mk)
 
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service.st \
-    android.hardware.secure_element@1.2.vendor
-
-PRODUCT_PACKAGES += \
-    libchrome.vendor
+    android.hardware.secure_element@1.2.vendor \
+    com.android.nfc_extras \
+    libchrome.vendor \
+    NfcNci \
+    SecureElement \
+    Tag
 
 # OMX
 PRODUCT_PACKAGES += \
