3200d79bc99faf1070f943d4123a59a6056b16a2
Roopesh Nataraja <quic_roopeshr@quicinc.com>
2023-04-21 09:08:10 +0530

    dubai: Enable compressed snapshots with virtual A/B
    Virtual A/B compression helps to reduce snapshot size on /data.
    Enable compression by inheriting virtual_ab_ota/compression.mk.
    
    This also reverts commit f5a6be19baa2a403523536a05e81416aa8a8c300.
    
    Change-Id: I47f241c966285243cdc379ce971c08603ffc575d

--- device.mk
+++ device.mk
@@ -3,11 +3,10 @@ DEVICE_PATH := device/motorola/dubai
 HARDWARE_PATH := hardware/motorola
 QCOM_COMMON_PATH := device/qcom/common
 
-# A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/android_t_baseline.mk)
-PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := gz
+# Enable virtual A/B compression
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
+# A/B related packages
 PRODUCT_PACKAGES += \
     update_engine \
     update_engine_client \
